19a2698d9bbb45fc625d19a92ce1f8aa74a448f8
bob <kranki@me.com>
2026-08-29 21:06:09 -0400

    Automated_Backup_Commit

--- remoteSetup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# vi:nu:et:sts=4 ts=4 sw=4
-
-
-git remote add apple04 ssh://apple04.local/Users/bob/data/gitBob/gitBob00/libCmn.git
-git remote add apple19 ssh://apple19.local/Users/bob/data/gitBob/gitBob00/libCmn.git
-
-
